732c47dc15bf34ac6b433334099f1c1b160abcca
niuting <1170146040@qq.com>
2019-10-24 15:17:14 +0800

    new project

---
+++ src/main/java/com/huya/community/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.huya.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author niuting
+ */
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index()
+    {
+        return "index" ;
+    }
+}
